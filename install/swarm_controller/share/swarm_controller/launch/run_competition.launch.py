21d06b58ee8438a02780df83345febcbe6c2283d
MohammadReza Hasanzadeh <m.r.h.k.2019@gmail.com>
2025-08-14 08:54:18 +0330

    SDF Chnge to run it in better way

--- install/swarm_controller/share/swarm_controller/launch/run_competition.launch.py
+++
@@ -1,1 +0,0 @@
-/home/mh/Documents/ros2_drone_swarm/build/swarm_controller/launch/run_competition.launch.py
--- install/swarm_msgs/lib/python3.12/site-packages/swarm_msgs/action/__init__.py
+++
@@ -1,7 +0,0 @@
-from swarm_msgs.action._execute_mission import ExecuteMission  # noqa: F401
-from swarm_msgs.action._execute_mission import ExecuteMission_GetResult_Event  # noqa: F401
-from swarm_msgs.action._execute_mission import ExecuteMission_GetResult_Request  # noqa: F401
-from swarm_msgs.action._execute_mission import ExecuteMission_GetResult_Response  # noqa: F401
-from swarm_msgs.action._execute_mission import ExecuteMission_SendGoal_Event  # noqa: F401
-from swarm_msgs.action._execute_mission import ExecuteMission_SendGoal_Request  # noqa: F401
-from swarm_msgs.action._execute_mission import ExecuteMission_SendGoal_Response  # noqa: F401
