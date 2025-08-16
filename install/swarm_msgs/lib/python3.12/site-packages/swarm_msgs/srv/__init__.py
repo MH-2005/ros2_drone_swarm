55c48c2626388550c4791c2587b93b8bfc65d649
MohammadReza Hasanzadeh <m.r.h.k.2019@gmail.com>
2025-08-16 17:23:02 +0330

    SDF Chnge to run it in better way

--- install/swarm_msgs/lib/python3.12/site-packages/swarm_msgs/srv/__init__.py
+++
@@ -1,8 +0,0 @@
-from swarm_msgs.srv._change_formation import ChangeFormation  # noqa: F401
-from swarm_msgs.srv._change_formation import ChangeFormation_Event  # noqa: F401
-from swarm_msgs.srv._change_formation import ChangeFormation_Request  # noqa: F401
-from swarm_msgs.srv._change_formation import ChangeFormation_Response  # noqa: F401
-from swarm_msgs.srv._disarm_leader import DisarmLeader  # noqa: F401
-from swarm_msgs.srv._disarm_leader import DisarmLeader_Event  # noqa: F401
-from swarm_msgs.srv._disarm_leader import DisarmLeader_Request  # noqa: F401
-from swarm_msgs.srv._disarm_leader import DisarmLeader_Response  # noqa: F401
